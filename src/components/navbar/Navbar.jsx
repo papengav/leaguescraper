64b92dbb9bca94df4ef41da7694b337dec72851e
Gavin Papenthien <papengav@gmail.com>
2023-05-03 13:00:56 -0500

    added src code dropdown to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,34 +5,22 @@ import logo from '../../assets/logo.png';
 const inviteLink = "https://discord.com/api/oauth2/authorize?client_id=1044453194413129819&permissions=8&scope=bot%20applications.commands"
 
 const Navbar = () => {
-  const scrollToOverview = () => {
-    window.scrollTo({
-      top: 100,
-      behavior: "smooth"
-    });
-  }
-
-  const scrollToAbout = () => {
-    window.scrollTo({
-      top: 1000,
-      behavior: "smooth"
-    });
-  }
-
-  const scrollToCommands = () => {
-    window.scrollTo({
-      top: 2000,
-      behavior: "smooth"
-    });
-  }
 
   return (
     <div className='navbar'>
       <div className='items'>
-        <img src={logo} alt='logo' className='logo' onClick={scrollToOverview}></img>
-        <p className='navbar-item' onClick={scrollToOverview}>LeagueScraper</p>
-        <p className='navbar-item' onClick={scrollToAbout}>About</p>
-        <p className='navbar-item' onClick={scrollToCommands}>Commands</p>
+        <img src={logo} alt='logo' className='logo'></img>
+        <p className='navbar-item'>LeagueScraper</p>
+        <p className='navbar-item'>About</p>
+        <p className='navbar-item'>Commands</p>
+        <div className='src-dropdown'>
+          <button className='src-btn'>Source Code</button>
+          <div className='src-dropdown-content'>
+            <a href='https://github.com/papengav/LeagueScraperDiscordBot'>Discord Bot</a>
+            <a href='https://github.com/papengav/leaguescraper'>Website</a>
+          </div>
+        </div>
+        <p className='src-arrow'>▼</p>
       </div>
       <a href = {inviteLink} target = "_blank" rel = "noreferrer">
         <button className='navbar-button'>Get LeagueScraper</button>
